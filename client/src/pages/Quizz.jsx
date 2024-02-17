bc9dc746f0f4ce141a71f5bbced8452acbaf347a
Alicia Saci <lyciiasci@gmail.com>
2024-02-17 21:19:03 +0100

    commit

--- client/src/pages/Quizz.jsx
+++ client/src/pages/Quizz.jsx
@@ -1,12 +1,28 @@
 import * as React from "react";
 import QuizzCard from "../components/QuizzCard";
+import Button from '@mui/material/Button';
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
+import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
+import ButtonGroup from '@mui/material/ButtonGroup';
+
+const buttons = [
+ <Button key="one">Précédent</Button>,
+ <Button key="two">Suivant</Button>,
+];
 
 const Quizz = () => {
   return (
-    <div style={{"display": "flex", "justify-content": "center", 'flex-wrap': 'wrap', 'margin-top' : '10rem'}}>
+   <div>
+      <div style={{"display": "flex", "justify-content": "center", 'flex-wrap': 'wrap', 'margin-top' : '10rem'}}>
      <h1 style={{"flex-basis" : "100%"}}>Quizz du jour</h1>
       <QuizzCard />
     </div>
+    <ButtonGroup color="secondary" aria-label="Medium-sized button group">
+        {buttons}
+      </ButtonGroup>
+   </div>
+  
+   
   );
 };
 
