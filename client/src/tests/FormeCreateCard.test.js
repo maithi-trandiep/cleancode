cf90b23f8f3fc61b293fb3e6d16e203e3bf76548
Alicia Saci <lyciiasci@gmail.com>
2024-02-20 20:35:50 +0100

    fix tests back

---
+++ client/src/tests/FormeCreateCard.test.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { render, fireEvent, waitFor, getByTestId } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import FormCreateCard from 'client/src/components/FormCreateCard';
+
+// Mock de CardService
+jest.mock('../service/CardService', () => ({
+  createCard: jest.fn(),
+}));
+
+describe('FormCreateCard component', () => { 
+  test('affiche le formulaire avec les éléments corrects', () => {
+    const { getByText } = render(<FormCreateCard />);
+
+    expect(getByText('Create new card')).toBeInTheDocument();
+  });
+
+ 
+});
