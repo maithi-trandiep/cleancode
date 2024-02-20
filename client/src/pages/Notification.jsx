30ce99b4183a43e417b0cc96c8cb758018d16181
maithi-trandiep <thi.tdm04@gmail.com>
2024-02-20 20:23:06 +0100

    some changes

---
+++ client/src/pages/Notification.jsx
@@ -0,0 +1,10 @@
+//import { DateTimePicker } from '@mui/x-date-pickers/DateTimePicker';
+
+const Notification = () => {
+    return (
+        <div>
+            {/* <DateTimePicker /> */}
+        </div>
+    );
+}
+export default Notification;
--- client/src/service/QuizService.jsx
+++ client/src/service/QuizService.jsx
@@ -2,11 +2,19 @@ import axios from "axios";
 
 export const QuizService = {
     async createQuiz(quiz) {
-        const response = await axios.post(`http://localhost:8080/quiz`, quiz);
-        return response.data;
+        try {
+            const response = await axios.post(`http://localhost:8080/quiz`, quiz);
+            return response.data;
+        } catch (error) {
+            return null;
+        }
     },
     async getQuizByUser(userId) {
-        const response = await axios.get(`http://localhost:8080/quiz/${userId}`);
-        return response.data;
+        try {
+            const response = await axios.get(`http://localhost:8080/quiz/${userId}`);
+            return response.data;
+        } catch (error) {
+            return null;
+        }
     }
 }
