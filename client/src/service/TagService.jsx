1792ed402318a505649093cc992030f5f402ab41
Londones <95936610+Londones@users.noreply.github.com>
2024-02-20 15:53:32 +0000

    merge tag and card services + fix tests

--- client/src/service/TagService.jsx
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-export const TagService = {
-    async getCardsByTags(tags) {
-        const response = await axios.get(`http://localhost:8080/cards?tags=${tags}`)
-        return response.data;
-    },
-};
--- client/src/tests/services/CardService.test.js
+++ client/src/tests/services/CardService.test.js
@@ -82,4 +82,26 @@ describe("CardService", () => {
 
     expect(response).toEqual(data);
   });
+
+  it("returns data when getCardsByTags is called", async () => {
+    const mock = new MockAdapter(axios);
+    const data = { response: true };
+    mock.onGet("http://localhost:8080/cards?tags=test").reply(200, data);
+
+    const response = await CardService.getCardsByTags("test");
+
+    expect(response).toEqual(data);
+  });
+
+  it("throws an error when getCardsByTags is called and request fails", async () => {
+    const mock = new MockAdapter(axios);
+    mock.onGet("http://localhost:8080/cards?tags=test").reply(500);
+
+    try {
+      await CardService.getCardsByTags("test");
+    } catch (error) {
+      expect(error).toBeDefined();
+      expect(error.response.status).toBe(500);
+    }
+  });
 });
